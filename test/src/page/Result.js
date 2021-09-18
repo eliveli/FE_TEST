994cae91635bce0ca11621a8fea4556a37a2efad
LAPPY\은혜 <elivevvv@gmail.com>
2021-09-19 03:24:55 +0900

    Feat: 데이터 로드 및 정렬

--- test/src/page/Result.js
+++ test/src/page/Result.js
@@ -1,7 +1,81 @@
 import React from "react";
+import {Button, Grid, Text} from "../elements";
+import Table from "../component/Table";
+import styled from "styled-components";
+import {Context, fetchData} from "../context/ResultContext";
 
 const Result = () => {
-  return <></>;
+
+  const {state, contextDispatch} = React.useContext(Context);
+
+  //result 목록 불러오기
+  const result = state.result;
+  React.useEffect(()=>{
+    if (state.result.length === 0){
+      fetchData(contextDispatch);
+    }
+  }, []);
+
+
+  //컬럼 별 오름차순/내림차순 정렬
+  const [upFox, handleUpFox] = React.useState(false);
+  const [upGolf, handleUpGolf] = React.useState(false);
+
+  const sort = (no, isUp) => {
+    result.sort(function(a, b) { 
+      if (isUp){
+        return a[no] - b[no]; 
+      } else {
+        return b[no] - a[no]; 
+      }
+    });
+  }
+  const sortFox = () => {
+    if (upFox){
+      handleUpFox(false);
+      sort(1, false);
+    } else{
+      handleUpFox(true);
+      sort(1, true);
+    }
+  }
+  const sortGolf = () => {
+    if (upGolf){
+      handleUpGolf(false);
+      sort(2, false);
+    } else{
+      handleUpGolf(true);
+      sort(2, true);
+    }
+  }
+  
+
+  return(
+  <Grid padding="10px" position="relative" top="50px" margin="0 auto" width="700px">
+    <Grid display="flex" justify="space-between" margin="10px">
+      <Text>Result</Text>
+      <Grid margin="0 10px 0 0" display="flex">
+        <Inp></Inp>
+        <Button>search</Button>
+        <Button margin="0 0 0 30px">download</Button>
+      </Grid>
+    </Grid>
+    
+    <Grid display="flex" justify="space-between" margin="10px">
+      <Text>Name</Text>
+      <Text _onClick={sortFox}>Foxtrot</Text>
+      <Text _onClick={sortGolf}>Golf</Text>
+    </Grid>
+    {result?.map(_=>
+      <Table _={_}></Table>
+    )}
+    
+  </Grid>
+  )
 };
+
+const Inp = styled.input`
+
+`
 
 export default Result;
