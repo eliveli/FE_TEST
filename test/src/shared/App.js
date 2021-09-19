148fa10e64fbc8a57f07fce00b5407528591b092
LAPPY\은혜 <elivevvv@gmail.com>
2021-09-19 20:30:57 +0900

    Feat: 최상단 이동 기능 추가

--- test/src/shared/App.js
+++ test/src/shared/App.js
@@ -1,7 +1,9 @@
 import React from "react";
 import Header from "./Header";
 import Result from "../page/Result";
-import {ContextProvider} from "../context/ResultContext"
+import {Grid} from "../elements";
+import {ContextProvider} from "../context/ResultContext";
+import { ReactComponent as GoTop } from "../image/GoTop.svg";
 
 function App() {
   return (
@@ -10,6 +12,18 @@ function App() {
       <ContextProvider>
         <Result></Result>
       </ContextProvider>
+
+      {/* 최상단 이동 버튼 */}
+      <Grid
+        position="fixed"
+        bottom="40px"
+        right="36px"
+        zIndex="1"
+        cursor={true}
+        _onClick={() => window.scrollTo(0, 0)}
+      >
+        <GoTop />
+      </Grid>
     </>
   );
 }
